5a5a83d1476d3f93a8fc0c1f72db019ec2370d63
Neil Immanuel De Guzman <deguzmanneilimmanuel@gmail.com>
2025-04-19 11:58:50 +0800

    CL - 2 move the default ignores inside list and allow to define the packages needed to be intercepted

--- src/main/java/com/odyss/codelens/agent/CodeLensAgent.java
+++ src/main/java/com/odyss/codelens/agent/CodeLensAgent.java
@@ -2,33 +2,43 @@ package com.odyss.codelens.agent;
 
 import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.asm.Advice;
-import net.bytebuddy.description.type.TypeDescription;
-import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.matcher.ElementMatchers;
-import net.bytebuddy.utility.JavaModule;
 
 import java.lang.instrument.Instrumentation;
-import java.security.ProtectionDomain;
+import java.util.Arrays;
+import java.util.List;
 
 public class CodeLensAgent {
 
-    public static void agentmain(String agentArgs, Instrumentation inst){
-        new AgentBuilder.Default()
-                .ignore(ElementMatchers.isSynthetic())
-                .or(ElementMatchers.nameStartsWith("net.bytebuddy."))
-                .or(ElementMatchers.nameStartsWith("sun."))
-                .or(ElementMatchers.nameStartsWith("com.sun."))
-                .or(ElementMatchers.nameStartsWith("jdk."))
-                .or(ElementMatchers.nameStartsWith("com.odyss.codelens"))
-                .type(ElementMatchers.any())
-                .transform((builder, typeDescription, classLoader, javaModule, protectionDomain) -> {
-                    return builder.method(ElementMatchers.any())
-                            .intercept(Advice.to(InterceptorAdvice.class));
-                })
-                .installOn(inst);
+    private static final List<String> DEFAULT_IGNORED_PACKAGES = Arrays.asList(
+            "sun.",
+            "com.sun.",
+            "jdk."
+    );
+
+    public static void agentmain(String agentArgs, Instrumentation inst) {
+        configureAgent(agentArgs, inst);
     }
 
-    public static void premain(String agentArgs, Instrumentation inst){
-        agentmain(agentArgs, inst);
+    public static void premain(String agentArgs, Instrumentation inst) {
+        configureAgent(agentArgs, inst);
+    }
+
+    private static void configureAgent(String agentArgs, Instrumentation inst) {
+        AgentBuilder agentBuilder = new AgentBuilder.Default()
+                .ignore(ElementMatchers.isSynthetic());
+
+        AgentBuilder.Ignored orAgentBuilder = agentBuilder.ignore(ElementMatchers.nameStartsWith("net.bytebuddy."));
+        for (String packageName : DEFAULT_IGNORED_PACKAGES) {
+            orAgentBuilder = orAgentBuilder.or(ElementMatchers.nameStartsWith(packageName));
+        }
+
+        AgentBuilder.Identified.Narrowable narrowableAgentBuilder = orAgentBuilder.type(ElementMatchers.any());
+        if (agentArgs != null && !agentArgs.trim().isEmpty()) {
+            narrowableAgentBuilder = narrowableAgentBuilder.and(ElementMatchers.nameStartsWith(agentArgs.trim()));
+        }
+        narrowableAgentBuilder.transform((builder, typeDescription, classLoader, javaModule, protectionDomain) -> builder.method(ElementMatchers.any())
+                        .intercept(Advice.to(InterceptorAdvice.class)))
+                .installOn(inst);
     }
 }
