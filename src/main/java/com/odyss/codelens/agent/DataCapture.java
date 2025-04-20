12cc512135fda2603c5ddbedabfa89133e0fc287
Neil Immanuel De Guzman <deguzmanneilimmanuel@gmail.com>
2025-04-20 09:17:22 +0800

    CL - 3 use Holder pattern for singleton

--- src/main/java/com/odyss/codelens/agent/DataCapture.java
+++ src/main/java/com/odyss/codelens/agent/DataCapture.java
@@ -2,6 +2,8 @@ package com.odyss.codelens.agent;
 
 import com.odyss.codelens.call.MethodCall;
 import com.odyss.codelens.call.Variable;
+import com.odyss.codelens.event.CallGraphEventManager;
+import com.odyss.codelens.event.EventManager;
 
 import java.lang.reflect.Parameter;
 import java.time.LocalDateTime;
@@ -9,16 +11,17 @@ import java.util.Arrays;
 
 public class DataCapture {
 
-    private static DataCapture instance;
     private final ThreadLocal<MethodCall> currentMethodCall = new ThreadLocal<>();
 
-    private DataCapture() {}
+    private static class Holder {
+        static final DataCapture INSTANCE = new DataCapture();
+    }
+
+    private DataCapture() {
+    }
 
     public static DataCapture getInstance() {
-        if (instance == null) {
-            instance = new DataCapture();
-        }
-        return instance;
+        return Holder.INSTANCE;
     }
 
     public void captureEntry(String className, String methodName, Parameter[] args) {
@@ -26,6 +29,7 @@ public class DataCapture {
                 .map(arg -> Variable.fromObject(arg.getName(), arg, LocalDateTime.now()))
                 .toList();
         currentMethodCall.set(new MethodCall(methodName, className, parameters));
+        CallGraphEventManager.getInstance().onMethodCaptureEntry(currentMethodCall.get());
     }
 
     public void captureExit(String className, String methodName, String type, Object value) {
@@ -33,6 +37,7 @@ public class DataCapture {
         if (methodCall != null) {
             methodCall.setEndTime(LocalDateTime.now());
             methodCall.setReturnedValue(Variable.fromObject("returnValue", value, LocalDateTime.now()));
+            CallGraphEventManager.getInstance().onMethodCaptureExit(methodCall);
             currentMethodCall.remove();
         }
     }
